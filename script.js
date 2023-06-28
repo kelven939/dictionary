5e47013f14b0b658f2801ca943c2ddf3dbde0c83
Kelven Bruno Carlos Lopes Bulha (KB) <112175768+kelven939@users.noreply.github.com>
2023-06-28 13:41:33 +0200

    update js scripts

--- script.js
+++ script.js
@@ -5,31 +5,31 @@ var dictionaryElement = document.getElementById("dictionary");
 function getDictionaryData() {
     var word = prompt("Digite uma palavra:"); // Solicita ao usuário uma palavra a ser pesquisada
 
-    // Realizar uma solicitação GET à API do Thesaurus.com
-    fetch("https://api.dictionaryapi.dev/api/v2/entries/en_US/" + word)
+    // Realizar uma solicitação GET à API do Big Huge Thesaurus
+    fetch("https://api.datamuse.com/words?ml=" + word)
         .then(response => response.json())
         .then(data => {
             // Limpar o conteúdo atual do dicionário
             dictionaryElement.innerHTML = "";
 
             // Verificar se a palavra foi encontrada
-            if (data.title) {
+            if (data.length > 0) {
+                // Iterar sobre as palavras sinônimas
+                data.forEach(synonym => {
+                    // Criar elementos para exibir cada sinônimo
+                    var synonymElement = document.createElement("p");
+                    synonymElement.textContent = synonym.word;
+
+                    // Adicionar os elementos ao dicionário
+                    dictionaryElement.appendChild(synonymElement);
+                });
+            } else {
                 // Se a palavra não for encontrada, exibir uma mensagem de erro
                 var errorElement = document.createElement("p");
                 errorElement.textContent = "Palavra não encontrada.";
 
                 // Adicionar o elemento de erro ao dicionário
                 dictionaryElement.appendChild(errorElement);
-            } else {
-                // Iterar sobre as definições da palavra
-                data[0].meanings.forEach(meaning => {
-                    // Criar elementos para exibir cada definição
-                    var definitionElement = document.createElement("p");
-                    definitionElement.innerHTML = "<strong>" + meaning.partOfSpeech + "</strong>: " + meaning.definitions[0].definition;
-
-                    // Adicionar os elementos ao dicionário
-                    dictionaryElement.appendChild(definitionElement);
-                });
             }
         })
         .catch(error => {
