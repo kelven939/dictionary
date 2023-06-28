34fae0348ec7d612751bac44d6b7ed70e31d9846
Kelven Bruno Carlos Lopes Bulha (KB) <112175768+kelven939@users.noreply.github.com>
2023-06-28 14:21:30 +0200

    add script

--- script.js
+++ script.js
@@ -5,36 +5,49 @@ var dictionaryElement = document.getElementById("dictionary");
 function getDictionaryData() {
     var word = prompt("Digite uma palavra:"); // Solicita ao usuário uma palavra a ser pesquisada
 
-    // Realizar uma solicitação GET à API do Big Huge Thesaurus
-    fetch("https://api.datamuse.com/words?ml=" + word)
-        .then(response => response.json())
-        .then(data => {
-            // Limpar o conteúdo atual do dicionário
-            dictionaryElement.innerHTML = "";
+    // Realizar uma solicitação GET à API do WordsAPI para obter sinônimos e traduções
+    fetch("https://google-translate1.p.rapidapi.com/language/translate/v2/detect" + word, {
+        method: "GET",
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+		'Accept-Encoding': 'application/gzip',
+		'X-RapidAPI-Key': 'dc6446a2admsh745128a0afca428p18b5e5jsn6a344212aafb',
+		'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Limpar o conteúdo atual do dicionário
+        dictionaryElement.innerHTML = "";
 
-            // Verificar se a palavra foi encontrada
-            if (data.length > 0) {
-                // Iterar sobre as palavras sinônimas
-                data.forEach(synonym => {
-                    // Criar elementos para exibir cada sinônimo
-                    var synonymElement = document.createElement("p");
-                    synonymElement.textContent = synonym.word;
+        // Verificar se a palavra foi encontrada
+        if (data.success && data.results) {
+            // Exibir os sinônimos no dicionário
+            if (data.results.synonyms) {
+                var synonymsElement = document.createElement("p");
+                synonymsElement.innerHTML = "<strong>Sinônimos:</strong> " + data.results.synonyms.join(", ");
 
-                    // Adicionar os elementos ao dicionário
-                    dictionaryElement.appendChild(synonymElement);
-                });
-            } else {
-                // Se a palavra não for encontrada, exibir uma mensagem de erro
-                var errorElement = document.createElement("p");
-                errorElement.textContent = "Palavra não encontrada.";
-
-                // Adicionar o elemento de erro ao dicionário
-                dictionaryElement.appendChild(errorElement);
+                dictionaryElement.appendChild(synonymsElement);
             }
-        })
-        .catch(error => {
-            console.log(error);
-        });
+
+            // Exibir as traduções no dicionário
+            if (data.results.translation) {
+                var translationElement = document.createElement("p");
+                translationElement.innerHTML = "<strong>Tradução:</strong> " + data.results.translation;
+
+                dictionaryElement.appendChild(translationElement);
+            }
+        } else {
+            // Se a palavra não for encontrada, exibir uma mensagem de erro
+            var errorElement = document.createElement("p");
+            errorElement.textContent = "Palavra não encontrada.";
+
+            dictionaryElement.appendChild(errorElement);
+        }
+    })
+    .catch(error => {
+        console.log(error);
+    });
 }
 
 // Chamar a função para solicitar a palavra ao usuário e exibir os resultados no dicionário
